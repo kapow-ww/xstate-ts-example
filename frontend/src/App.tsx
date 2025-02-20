1a2bc0242288df7ec6e9092030fa37965bc21cd0
natthanonC <nathanon.cha@dplusonline.net>
2025-02-21 00:18:08 +0700

    add theme switcher

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 import ToggleButton from "./components/toggle-button";
+import ChangeThemeToggle from "./components/change-theme-button";
 
 function App() {
   return (
     <div className="max-w-5xl mx-auto h-screen">
+      <ChangeThemeToggle />
       <div className="flex justify-center items-center flex-col gap-8 h-full">
         <ToggleButton />
       </div>
